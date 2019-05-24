9eab40c0e8fd3900b872c4da8f68632efb5ea73d
Ben <ben@zfben.com>
2019-05-24 16:58:09 +0800

    fix config

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,15 @@ import Cookie from './cookie';
 import Flow from '@faasjs/flow';
 
 export function handler (opts: {
-  resource: {
-    config: {
-      session?: {
-        key: string;
-        secret: string;
-        [key: string]: any;
-      };
+  config: {
+    session?: {
+      key: string;
+      secret: string;
       [key: string]: any;
     };
     [key: string]: any;
   };
   [key: string]: any;
 }, flow: Flow) {
-  return new Cookie(opts.resource.config, flow.helpers);
+  return new Cookie(opts.config, flow.helpers);
 }
